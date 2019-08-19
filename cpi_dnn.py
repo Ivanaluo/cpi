a8b9b6a5f300cfc3f900a316e6e57c393f2cb55e
ivan <51904290+Ivanaluo@users.noreply.github.com>
2019-08-19 17:33:40 +0800

    Update cpi_dnn.py

--- cpi_dnn.py
+++ cpi_dnn.py
@@ -2,16 +2,16 @@
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.bn_input = torch.nn.BatchNorm1d(384, momentum=0.8)   # (batch, 384)
-        self.conv1 = nn.Sequential(        # input shape (batch, 1, 384)
+        self.bn_input = torch.nn.BatchNorm1d(384, momentum=0.8)   
+        self.conv1 = nn.Sequential(       
             nn.Conv1d(
                 in_channels=1,
                 out_channels=30,
                 kernel_size=1,
                 stride=1,
-                padding=0),          #  padding=(kernel_size-1)/2 , output shape (batch, 30, 384)
+                padding=0),         
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2))    # 在 1*1 空间里向下采样, output shape (batch, 30, 192)
+            nn.MaxPool1d(kernel_size=2))   
         self.conv2 = nn.Sequential(
             nn.Conv1d(
                 in_channels=30,
@@ -20,19 +20,19 @@ class Net(torch.nn.Module):
                 stride=1,
                 padding=0),   
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2))    # output shape (batch, 1, 96)
+            nn.MaxPool1d(kernel_size=2))   
             
-        self.encoder = nn.Sequential(         # (batch, 96)
+        self.encoder = nn.Sequential(        
             nn.Linear(96, 64),
             nn.BatchNorm1d(64, momentum=0.5),
             # nn.Dropout(0.5),
             nn.LeakyReLU(inplace=True),
-            nn.Linear(64, 48))                # (batch, 48)
+            nn.Linear(64, 48))             
         
         for p in self.parameters():
             p.requires_grad = False
             
-        self.rnn1 = nn.Sequential(            # (batch, 1, 48)
+        self.rnn1 = nn.Sequential(          
             nn.LSTM(
             input_size = 48,
             hidden_size = 32,
